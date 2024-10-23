604e109fc01d03bef7affd4794fb3b1a3757da5e
ismycris <cristielythainara@gmail.com>
2024-10-23 15:41:23 -0300

    update

---
+++ src/app/components/vendas/venda-form/venda-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-venda-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './venda-form.component.html',
+  styleUrl: './venda-form.component.scss'
+})
+export class VendaFormComponent {
+
+}
