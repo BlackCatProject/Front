0e50dca18d4ea14cc51c4386879f13ff0b67aa3c
willkiwi <contatowillpm@gmail.com>
2024-10-25 22:28:55 -0300

    read e desativar OK, create e update OFF por enquanto

--- src/app/components/usuarios/usuario-form/usuario-form.component.ts
+++ src/app/components/usuarios/usuario-form/usuario-form.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Input, input, Output } from '@angular/core';
 import Swal from 'sweetalert2';
 import { Usuario } from '../../../models/usuario';
 import { UsuarioService } from '../../../services/usuario.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-form',
@@ -13,82 +14,19 @@ import { UsuarioService } from '../../../services/usuario.service';
 
 export class UsuarioFormComponent {
 
-  nome: string = '';
-  login: string = '';
-  senha: string = '';
-  role: 'GESTOR' | 'FUNCIONARIO' = 'FUNCIONARIO';
+  routter = inject(ActivatedRoute);
+  @Input("usuario") usuario: Usuario = new Usuario();
+  @Output("retorno") retorno = new EventEmitter();
 
 
-  constructor(private usuarioService: UsuarioService) {}
+  constructor () { }
 
-  cadastrarUsuario(): void {
-    const novoUsuario: Usuario = {
-      id : 0,
-      nome: this.nome,
-      login: this.login,
-      senha: this.senha,
-      role: this.role,
-      ativo: true};
-    this.usuarioService.salvarUsuario(novoUsuario).subscribe({
-      next: mensagem => {
-        Swal.fire({
-          title: mensagem,
-          icon: 'success',
-          timer: 1500,
-          confirmButtonText: 'Ok',
-        });
-        this.limparCampos();
-      },
+  save() {
+    alert("usuario salvo com sucesso!" + this.usuario.nome);
+    this.retorno.emit(this.usuario);
+  }
 
-      error: error => {
-        Swal.fire({
-          title: 'Erro ao cadastrar usuário',
-          icon: 'error',
-          timer: 1500,
-          confirmButtonText: 'Ok',
-        });
-        console.error('Erro ao cadastrar usuário:', error);
-      }
-    });
-    }
-
-    atualizarUsuario(id: number): void {
-      this.usuarioService.findById(id).subscribe({
-        next: (usuario) => {
-          this.preencherCampos(usuario);
-          Swal.fire({
-            title: 'Usuário carregado para edição.',
-            icon: 'success',
-            timer: 1500,
-            confirmButtonText: 'Ok',
-          })
-        },
-        error: (error) => {
-          console.error('Erro ao atualizar usuário:', error);
-          if (error.status === 404) {
-            Swal.fire({
-              title: 'Usuário não encontrado',
-              icon: 'error',
-              timer: 1500,
-              confirmButtonText: 'Ok',
-            });
-          }
-        },
-      });
-    }
-
-    preencherCampos(usuario: Usuario): void {
-      this.nome = usuario.nome;
-      this.login = usuario.login;
-      this.senha = usuario.senha;
-      this.role = usuario.role;
-      // Você pode querer lidar com outros campos conforme necessário
-    }    
-
-    limparCampos(): void {
-      this.nome = '';
-      this.login = '';
-      this.senha = ''; 
-      this.role = 'FUNCIONARIO'; 
-    }
+  
+  
+  
 }
