12b9938fa95ee219feb69d166566b8c1e8db4348
willkiwi <contatowillpm@gmail.com>
2024-11-21 22:46:39 -0300

    smtei xD

---
+++ src/app/components/layout/dashboard-funcionario/dashboard-funcionario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-funcionario',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-funcionario.component.html',
+  styleUrl: './dashboard-funcionario.component.scss'
+})
+export class DashboardFuncionarioComponent {
+
+}
